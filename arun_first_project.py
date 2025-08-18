9cfe3cbd2022f0c8b16eb5e728fb13271d486b04
Arunkumar <arunkumarpgowda1999@gmail.com>
2025-08-18 18:17:44 +0530

    arunlearn

--- arun_first_project.py
+++ arun_first_project.py
@@ -1,12 +1,1 @@
-def sum(a,b):
-    c=a+b
-    return c
-print("checking when pull")
-
-d=sum(5,6)
-print(d)
-
-print("learning")
-print("kali")
-print("testitn")
-print("deploying to arun branch")
+"test"
